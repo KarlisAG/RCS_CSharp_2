f796a2dc1ce2548cf0bb3c9a7ba9caa8dfe4b0ed
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-04-27 17:14:11 +0300

    Diena4_Nodarbība

---
+++ Diena4_(LoopExceptions)CikluIzņēmumi/Diena4_(LoopExceptions)CikluIzņēmumi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Diena4__LoopExceptions_CikluIzņēmumi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
