08ce680ba21768bfc51c8f1abf4b56a83fd8b83e
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-04-28 02:03:37 +0300

    Diena 4 MD

---
+++ Day4_MD_Kārlis/Day4_MD_Kārlis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day4_MD_Kārlis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
