417d89becf88c12bb5381958bd5110f0673ca84a
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-04-27 17:57:52 +0300

    Update Program.cs
    komentāri

--- Diena4_(LoopExceptions)CikluIzņēmumi/Diena4_(LoopExceptions)CikluIzņēmumi/Program.cs
+++ Diena4_(LoopExceptions)CikluIzņēmumi/Diena4_(LoopExceptions)CikluIzņēmumi/Program.cs
@@ -37,20 +37,24 @@ namespace Diena4__LoopExceptions_CikluIzņēmumi
         static void TestParse()
         {
             int sk;
-
+            //strādā līzīgi kā IF
             try //šo darīs līdz būs exception //// iziet cauri vienreiz - pa defu
             {
                 Console.WriteLine("Ieavadiet skaitli");
                 sk = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Tikām tālāk");
             }
-            catch (Exception ex) //šajā gad. ķer visus expection, neatkarīgi ne no kā, bet var arī specifiskus)
+            catch (Exception ex) //varī arī bez ex, viss ok strādās; to liek ja grib (skat. IV)
+            //šajā gad. ķer visus expection, neatkarīgi ne no kā, bet var arī specifiskus)
             {//catch iziet vienreiz cauri un tad pasaka erro un programma beidz darbīu, ja netaisa izņēmumus taisīt kādu citu loop
+                //try gadījumā var bool return true un catch rreturn false!!
+                // (IV) Console.WriteLine(ex.ToStrinh/Message utt - var saglabāt error, redzēt kas un kā notika
+                
                 sk = 0;
                 Console.WriteLine("Exceptions!");
                 //TestParse(); --> šī ir rekursija - ja ir exception, tad iet atkal metode, bet tas ir mūžīgais cikls
-            }
-
+            } 
+            //FormatExceptio, OverflowException - ķīer kkādu konkrētu --> Exception - ķer un var rādīt visus; atkarīgs ko un kā grib
             Console.WriteLine("Succes " + sk); //ja ar rekursiju taisa, tad šo ieliek try blokā
         }
     }
