583f2cfb62bc8cbe8eb75af47b8b12865d1e6239
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-04-29 02:26:23 +0300

    Update Program.cs
    1.uzdevums gatavs

--- Day4_MD_Kārlis/Day4_MD_Kārlis/Program.cs
+++ Day4_MD_Kārlis/Day4_MD_Kārlis/Program.cs
@@ -6,7 +6,49 @@ namespace Day4_MD_Kārlis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Kāpināšana();
+            //RandomNumber();
+        }
+
+        static void RandomNumber()
+        {
+            Random rnd = new Random();
+            
+            for (int i = 4; i > -1; i--)
+            {
+                Console.WriteLine("Cenšaties uzminēt random skaitli diapazonā no 1 līdz 20!");
+                String ievade = Console.ReadLine();
+                int ievadeC = Convert.ToInt32(ievade);
+                int randomC = rnd.Next(1, 20);
+                Console.WriteLine(randomC);
+                if(ievadeC == randomC)
+                {
+                    Console.WriteLine("Apsveicam, Jūs uzminējāt pareizo ciparu!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Jūs diemžēl neuzminējāt, mēģinet vēlreiz. Jums vēl ir palikuši " + i + " mēģinājumi!");
+                }
+
+            }
+        }
+
+        static void Kāpināšana()
+        {
+
         }
     }
 }
+/*1) Patstāvīgi atrast kā ģenerēt random skaitli. Lietotājs cenšas uzminēt skaitli piecas reizes, ja neizdodas, tad zaudē, ja izdodas, tad uzvar.
+
+
+2) Izveidot metodi, kas padod divus int tipa skaitļus kā parametrus- bāzi un pakāpi. Metode atgriež kāpinājumu.
+Piemērs, padod 2 un 3, tātad 2^3 = 2* *2 * 2 = 8
+
+3) Izvadīt šādu piramīdu, izmantojot for ciklu.
+1
+2 3
+4 5 6
+7 8 9 10
+*/
