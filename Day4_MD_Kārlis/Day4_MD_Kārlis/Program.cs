83b724fe7153972c395e88b86a8a091f00192d3f
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-04-29 12:18:21 +0300

    Update Program.cs
    Visi uzdevumi pabeigti

--- Day4_MD_Kārlis/Day4_MD_Kārlis/Program.cs
+++ Day4_MD_Kārlis/Day4_MD_Kārlis/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Dynamic;
 
 namespace Day4_MD_Kārlis
 {
@@ -6,8 +7,15 @@ namespace Day4_MD_Kārlis
     {
         static void Main(string[] args)
         {
-            Kāpināšana();
-            //RandomNumber();
+            RandomNumber();
+            Console.WriteLine("\n");
+            Console.WriteLine("Ierakstiet, lūdzu, divus dažādus ciparus, kurus vēlaties kāpināt. Pirmais cipars ir bāzes un otrais kāpinātāja vērtība!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kāpināšanas vērtība ir: " + KāpināšanaFinal(a, b));
+            Console.WriteLine("\n");
+            Console.WriteLine("Zemāk ir apskatāma īsa ciparu piramīda!");
+            Piramīda();
         }
 
         static void RandomNumber()
@@ -20,7 +28,7 @@ namespace Day4_MD_Kārlis
                 String ievade = Console.ReadLine();
                 int ievadeC = Convert.ToInt32(ievade);
                 int randomC = rnd.Next(1, 20);
-                Console.WriteLine(randomC);
+                //Console.WriteLine(randomC);
                 if(ievadeC == randomC)
                 {
                     Console.WriteLine("Apsveicam, Jūs uzminējāt pareizo ciparu!");
@@ -28,27 +36,71 @@ namespace Day4_MD_Kārlis
                 }
                 else
                 {
-                    Console.WriteLine("Jūs diemžēl neuzminējāt, mēģinet vēlreiz. Jums vēl ir palikuši " + i + " mēģinājumi!");
+                    Console.WriteLine("Jūs diemžēl neuzminējāt, Jums vēl ir palikuši " + i + " mēģinājumi!");
+                    if (i == 0)
+                    {
+                        Console.WriteLine("Jūsu mēģinājumi ir diemžēl beigušies, lai mēģinātu atkal pārrestartējiet programmu!");
+                    }
                 }
 
             }
         }
 
-        static void Kāpināšana()
+        static int KāpināšanaFinal(int a, int b)
         {
+            double jaunaisA = Convert.ToDouble(a);
+            double jaunaisB = Convert.ToDouble(b);
+            double result;
+            result = Math.Pow(jaunaisA, jaunaisB);
+            int resultI = Convert.ToInt32(result);
+            return resultI;
 
+        }
+
+        /*puspabeigts kods, jo nesanāca vēlamais rezultāts.. Vai ir iespēja mainīt mainīgā vērtības pārejot no if uz else statement? Jo mana ideja bija 1. reizē a*a un tad tālāk reizilāt to rezultātu ar tik reizes cik nepieciešams, bet vienmēr saskāros ar problēmu, ka mainīgajam vērtība nav atjaunota.. Kā būtu bijis labāk darīt?
+         * static int Kāpināšana(int a, int b)
+        {
+            int pēdējais;
+            
+            //Math mat = new Math();
+            //int kap = Math.Exp(b);
+            //Console.WriteLine(kap);
+            //return kap;
+            for(int i = 0; i == b; i++)
+            {
+                
+                if (i == 0)
+                {
+                    Kāpināšana2(a);
+                }
+                else 
+                {    
+                    pēdējais = Kāpināšana2(a) * a;
+                    Console.WriteLine(pēdējais);
+                    return pēdējais;
+                }
+
+            }
+            return pēdējais;
+        }
+
+        static int Kāpināšana2(int a)
+        {
+            int jaunaisA = a * a;
+            return jaunaisA;
+        }
+        */
+        static void Piramīda()
+        {
+            int cipars = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(++cipars + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
-/*1) Patstāvīgi atrast kā ģenerēt random skaitli. Lietotājs cenšas uzminēt skaitli piecas reizes, ja neizdodas, tad zaudē, ja izdodas, tad uzvar.
-
-
-2) Izveidot metodi, kas padod divus int tipa skaitļus kā parametrus- bāzi un pakāpi. Metode atgriež kāpinājumu.
-Piemērs, padod 2 un 3, tātad 2^3 = 2* *2 * 2 = 8
-
-3) Izvadīt šādu piramīdu, izmantojot for ciklu.
-1
-2 3
-4 5 6
-7 8 9 10
-*/
