f7cc4b4dafbb59830db4500aacbbac4733268515
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-06 17:26:45 +0300

    Classes#1

--- Diena6_(Classes)Klases/Diena6_(Classes)Klases/Program.cs
+++ Diena6_(Classes)Klases/Diena6_(Classes)Klases/Program.cs
@@ -6,13 +6,19 @@ namespace Diena6__Classes_Klases
     {
         static void Main(string[] args)
         {
-            Example.Test(); //sākumā klases nos. un tad metodes nos; no kurienes ko sauc -> tas ir ja metode ir statiska
+            Console.WriteLine("Lūdzu, ievadiet vienu skaitli!");
+            int ievadeC = Convert.ToInt32(Console.ReadLine());
+            Tasks.Task1(ievadeC);
+            
+            /*
+             * Example.Test(); //sākumā klases nos. un tad metodes nos; no kurienes ko sauc -> tas ir ja metode ir statiska
             int a = Example.ReturnPublic();
             Console.WriteLine(a);
 
             Example.ParamPublic(4, 6);
 
             Example.b = 8;
+            */
         }
 
 
---
+++ Diena6_(Classes)Klases/Diena6_(Classes)Klases/Tasks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diena6__Classes_Klases
+{
+    class Tasks
+    {
+        public static void Task1(int a)
+        {
+            String zvaigzne = "";
+            for (int i = 0; i < a; i++)
+            {
+                zvaigzne = zvaigzne + "*";
+                Console.WriteLine(zvaigzne);
+            }
+        }
+    }
+}
