13062ea83e7df2cf9bdb382daf8191ddac647c8c
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-07 20:31:29 +0300

    Diena 6 mājasdarbs
    Task 1 gatavs

---
+++ Day6_MD/Day6_MD/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day6_MD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students.Studenti();
+            Students.print();
+        }
+    }
+}
+/*1) Izveidot klasi students
+Lauki- vards, uzvards, kurss.
+Uztaisīt tā, lai cilvēks ievada studenta vārdu, uzvārdu, kursu, kursam jābūt no 1-3.
+Izveidot klasē metodi (publisku) print(), kas izvada visus laukus.
+
+2) Izveidot kalkulatoru-
+Visas ievades/izvades notiek Program.cs klasē, visi aprēķini jāizveido kā metodes ar parametriem un return vērtībām
+*/
---
+++ Day6_MD/Day6_MD/Students.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6_MD
+{
+    class Students
+    {
+        private static String vārds;
+        private static String uzvārds;
+        private static String kurss;
+
+        public static void Studenti()
+        {
+            Console.WriteLine("Lūdzu, ievadiet savu vārdu!");
+            setVārds(Console.ReadLine());
+            Console.WriteLine("Lūdzu, ievadiet savu uzvārdu!");
+            setUzvārds(Console.ReadLine());
+            Console.WriteLine("Lūdzu, ievadiet sev vēlamo kursu (1-3)");
+            setKurss(Console.ReadLine());
+        }
+
+        public static void setVārds(String a)
+        {
+            vārds = a;
+        }
+
+        public static String getVārds()
+        {
+            return vārds;
+        }
+        public static void setUzvārds(String a)
+        {
+            uzvārds = a;
+        }
+
+        public static String getUzvārds()
+        {
+            return uzvārds;
+        }
+        public static void setKurss(String a)
+        {
+            kurss = a;
+        }
+
+        public static String getKurss()
+        {
+            return kurss;
+        }
+
+        public static void print()
+        {
+            Console.WriteLine("Jūsu vārds ir " + getVārds() + " un uzvārds ir " + getUzvārds() + ", un Jūs esat izvēlējušies " + getKurss() + ". kursu!");
+        }
+    }
+    
+}
