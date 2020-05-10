00ef48456a57a45e5a05e9d1e4db1aa277053ce2
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-10 13:08:36 +0300

    Diena 7 nodarbība

---
+++ Day7_(Objects)Objekti.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day7__Objects_Objekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //statiskas metodes taisa, tad kad netaisa objektu??
+            Trijsturis tr1 = new Trijsturis();
+            tr1.Print();//ja nav statiskas metodes, tad rakstot klasi un metodes, tad neko nevar izsaukt jeb Trijsturi. .. neder!
+            //^^ bet rakstot objektu var tikt klāt!!
+
+
+        }
+    }
+}
