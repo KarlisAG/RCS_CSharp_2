dab56ef7b3c010786d348db53d93f7e7ebc5c27f
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-11 13:49:55 +0300

    Day 7 MD
    izpildīts uzdevums ar pasniedzēja piemēru

--- Day7_MD/Day7_MD/Program.cs
+++ Day7_MD/Day7_MD/Program.cs
@@ -7,92 +7,67 @@ namespace Day7_MD
     {
         static void Main(string[] args)
         {
-            List2();
+            MD();
         }
 
-        static void List2()
+        static void MD()
         {
-            List<int> l = new List<int>();
             Random random = new Random();
+
+            List<int> numbers = new List<int>();
+
             for (int i = 0; i < 10; i++)
             {
-                l.Add(random.Next(51));
-
+                numbers.Add(random.Next(100));
             }
-            bool stop = false;
-            while (stop == false)
+
+            String choice = "";
+            while (choice != "0")
             {
-                Console.WriteLine();
-                Console.WriteLine("Sveiki! Ko Jūs vēlaties izdarīt?");
-                Console.WriteLine("Uzspiežot 1 - izvadīt sarakstu");
-                Console.WriteLine("Uzspiežot 2 - izdzēst kādu elementu no saraksta pēc indeksa");
-                Console.WriteLine("Uzspiežot 3 - iziet no šīs programmas");
+                Console.WriteLine("1 - izvadīt sarakstu");
+                Console.WriteLine("2 - dzēst");
+                Console.WriteLine("0 - iziet");
 
-                String ievade = Console.ReadLine();
-                if (ievade == "1")
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Dotā brīža saraksts: ");
-                    foreach (int a in l)
-                    {
-                        Console.WriteLine(a);
-                    }
+                choice = Console.ReadLine();
 
-                }
-                else if (ievade == "2")
+                switch (choice)
                 {
-                    Console.WriteLine();
-                    Console.WriteLine("Ievadiet, kāda indeksa skaitli Jūs vēlaties izdzēst");
-
-                    try
-                    {
-                        int remove = Convert.ToInt32(Console.ReadLine());
-                        l.RemoveAt(remove);
-                    }
-                    catch (ArgumentOutOfRangeException)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Norādītais indekss sarakstā nav pieejams!");
-                    }
-                }
-                else if (ievade == "3")
-                {
-                    Stop();
-                    stop = true;
-                }
-                else
-                {
-                    Console.WriteLine(); 
-                    Console.WriteLine("Jūs esat ievadījuši nepareizu simbolu, lūdzu, ievadiet no 1 līdz 3!");
+                    case "1":
+                        foreach (int a in numbers)
+                        {
+                            Console.WriteLine(a);
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("Kuru dzēst?");
+                        int toDelete = Convert.ToInt32(Console.ReadLine());
+                        try
+                        {
+                            numbers.RemoveAt(toDelete);
+                        }
+                        catch(ArgumentOutOfRangeException)
+                        {
+                            Console.WriteLine("Ievadītais indekss nav atrodams sarakstā!");
+                        }
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("nepareiza ievade!");
+                        break;
                 }
 
-                if (l.Count == 0)
+                if(numbers.Count == 0)
                 {
-                    Console.WriteLine();
                     Console.WriteLine("Saraksts ir tukšs!");
-                    Console.WriteLine("Vai vēlaties programmu sākt no jauna? Ja vēlaties, tad uzrakstiet Y");
-                    String ievadeBeigt = Console.ReadLine();
-                    ievadeBeigt.ToLower();
-                    if (ievadeBeigt == "y")
-                    {
-                        List2();
-                    }
-                    else
-                    {
-                        Stop();
-                        stop = true;
-                    }
+                    break;
                 }
             }
         
+
         }
 
-        static void Stop()
-        {
-            Console.WriteLine("Paldies, ka lietojā šo programmu!");
-        }
-        
     }
-       
 }
+
 
