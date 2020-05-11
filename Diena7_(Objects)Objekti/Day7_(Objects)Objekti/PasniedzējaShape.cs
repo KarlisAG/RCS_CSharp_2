8891a3be3c64cdb3ead92a69b69c239f3003784e
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-11 04:16:37 +0300

    Diena 7 nodarbība
    Pasniedzēja Object2 variants

---
+++ Diena7_(Objects)Objekti/Day7_(Objects)Objekti/PasniedzējaShape.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day7__Objects_Objekti
+{
+    class PasniedzējaShape
+    {
+        private double a, b, c, d, e;
+
+        public PasniedzējaShape(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = 0;
+            this.e = 0;
+        }
+        public PasniedzējaShape(double a, double b, double c, double d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.e = 0;
+        }
+        public PasniedzējaShape(double a, double b, double c, double d, double e)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.e = e;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine();
+            Console.Write(a + "," + b + "," + c);//WriteLine atsarpi liek pēc ierakstītā, ne pirms tā
+            if (d > 0)
+            {
+                Console.Write("," + d);
+            }
+
+            if (e > 0)
+            {
+                Console.Write("," + e);
+            }
+        }
+
+        public double Perimeter()//ne tikai izvada perimetru (ja būtu void), bet to atgriež [labāk lietojama progr]; labāk klasē; un GUI
+        {
+            return a + b + c + d + e;
+        }
+    }
+}
--- Diena7_(Objects)Objekti/Day7_(Objects)Objekti/Program.cs
+++ Diena7_(Objects)Objekti/Day7_(Objects)Objekti/Program.cs
@@ -7,6 +7,14 @@ namespace Day7__Objects_Objekti
     {
         static void Main(string[] args)
         {
+            //IzvadeObject2();
+            //Shape daudzstūris = new Shape(3,4,5, 6);
+            PasniedzējaTask2();
+            
+        }
+
+        static void NodarbībasSākums()
+        {
             //ja ir static - tad objektu nelieto; statiskai vērtībai nevir piešķirt, to aks ir apredzēts objektam
             //statiskas metodes taisa, tad kad netaisa objektu??
             Trijsturis tr1 = new Trijsturis(1, 2, 3);//metodei jeb konstruktoram vajag tad uzreiz parametrus
@@ -30,13 +38,6 @@ namespace Day7__Objects_Objekti
             List<Students> st = new List<Students>();//ja ir runa par objektiem, tad list ir daudz labāks
                                                      //ja uztaisa for ciklu uz i, ja arrayiem nav viss aizpildīs, tad crash
                                                      //listi nekrashos, garums pielāgosies; un var vienkārši add pie listes un būs ok
-
-            Console.WriteLine("Lūdzu, ievadiet sava daudzstūra vēlamo malu garumus! Jums ir jāievada 3-5 dažādi cipari. Ja gribat tikai trīsstūri vai četrstūri, tad ievadiet tik dažādu malu garumus un pārējās ievadēs (kopā 5) ievadet jebkuru citu simbolu, atskaitot ciparus!");
-            Shape daudzstūrisArIevadi = new Shape(Input(), Input(), Input(), Input(), Input());
-            Shape daudzstūris = new Shape(3,4,5, 6);
-            daudzstūrisArIevadi.Print();
-            daudzstūrisArIevadi.Perimeter();
-
         }
 
         static int Input()
@@ -52,6 +53,48 @@ namespace Day7__Objects_Objekti
                 return 0;
             }
 
+        }
+        static void IzvadeObject2()
+        {
+            Console.WriteLine("Lūdzu, ievadiet sava daudzstūra vēlamo malu garumus! Jums ir jāievada 3-5 dažādi cipari. Ja gribat tikai trīsstūri vai četrstūri, tad ievadiet tik dažādu malu garumus un pārējās ievadēs (kopā 5) ievadet jebkuru citu simbolu, atskaitot ciparus!");
+            Shape daudzstūrisArIevadi = new Shape(Input(), Input(), Input(), Input(), Input());
+            daudzstūrisArIevadi.Print();
+            daudzstūrisArIevadi.Perimeter();
+        }
+
+        static void PasniedzējaTask2()
+        {
+            Console.WriteLine("Kādu figūru vēlaties?");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            PasniedzējaShape sh;
+
+            if(choice == 3)
+            {
+                sh = new PasniedzējaShape(4, 5, 6);
+                
+            }else if (choice == 4)
+            {
+                sh = new PasniedzējaShape(4, 7, 3, 2);
+            }
+            else
+            {
+                return;
+            }
+
+
+                Console.WriteLine(sh.Perimeter());
+                sh.Print();
+
+
+            /*PasniedzējaShape tr = new PasniedzējaShape(4, 5, 6);
+            Console.WriteLine(tr.Perimeter());
+            tr.Print();
+
+            PasniedzējaShape cetr = new PasniedzējaShape(4, 4, 4, 4);
+            Console.WriteLine(cetr.Perimeter());
+            cetr.Print();
+            */
         }
     }
 }
