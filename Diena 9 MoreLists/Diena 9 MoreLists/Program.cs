8808d0280e51115b39380496c4dd4f1ddeac1896
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-13 17:39:06 +0300

    List of Objects

---
+++ Diena 9 MoreLists/Diena 9 MoreLists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Diena_9_MoreLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Diena 9 MoreLists/Diena 9 MoreLists/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diena_9_MoreLists
+{
+    class Student
+    {
+    }
+}
