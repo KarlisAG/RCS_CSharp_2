03b0b41096e20f63c0edbe79249e133ab8e5f8dc
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-13 18:05:19 +0300

    Diena 9
    Task 1 gatavs

--- Diena 9 MoreLists/Diena 9 MoreLists/Program.cs
+++ Diena 9 MoreLists/Diena 9 MoreLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Diena_9_MoreLists
 {
@@ -6,7 +7,69 @@ namespace Diena_9_MoreLists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Example1();
+            
+
+            
+        }
+
+        static void Sample()
+        {
+            List<Student> listOfStudents = new List<Student>();
+
+            listOfStudents.Add(new Student("Janis", "Berzins", 1));
+            listOfStudents.Add(new Student("Juris", "Abele", 2));
+            listOfStudents.Add(new Student("Kristaps", "Kalejs", 3));
+
+            for(int i = 0; i < listOfStudents.Count; i++)
+            {
+                listOfStudents[i].print();
+                listOfStudents[i].setCourse(1);//visiem uzliek 1. kursu
+            }
+
+            foreach (Student st in listOfStudents)
+            {
+                st.print();
+            }
+        }
+        static void Example1()
+        {
+            List<Student> listOfStudents = new List<Student>();
+            String ievade = "";
+
+            while (ievade != "0")
+            {
+                Console.WriteLine("Sveiki! Ko Jus velaties izdarit?");
+                Console.WriteLine("1 - izvadit studentu sarakstu");
+                Console.WriteLine("2 - pievienot jaunu studentu saraksta");
+                Console.WriteLine("0 - iziet no programmas");
+                ievade = Console.ReadLine();
+
+                switch(ievade)
+                {
+                    case "1":
+                        foreach(Student st in listOfStudents)
+                        {
+                            st.print();
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("Ievadiet studenta vardu!");
+                        String inName = Console.ReadLine();
+                        Console.WriteLine("Ievadiet studenta uzvardu!");
+                        String inSurname = Console.ReadLine();
+                        Console.WriteLine("Ievadiet studenta kursu!");
+                        int inCourse = Convert.ToInt32(Console.ReadLine());
+                        listOfStudents.Add(new Student (inName, inSurname, inCourse));
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("Ievades kluda!");
+                        break;
+                }
+            }
+
         }
     }
 }
--- Diena 9 MoreLists/Diena 9 MoreLists/Student.cs
+++ Diena 9 MoreLists/Diena 9 MoreLists/Student.cs
@@ -6,5 +6,62 @@ namespace Diena_9_MoreLists
 {
     class Student
     {
+        public Student(String name, String surname, int course)
+        {
+            this.name = name;
+            this.surname = surname;
+            setCourse(course);
+        }
+
+        private String name;
+        private String surname;
+        private int course;
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public void setSurname(String surname)
+        {
+            this.surname = surname;
+        }
+
+        public void setCourse(int course)
+        {
+            if (course > 3)
+            {
+                course = 3;
+            }
+
+            if (course < 1)
+            {
+                course = 1;
+            }
+
+            this.course = course;
+        }
+
+        public void print()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(surname);
+            Console.WriteLine(course);
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public String getSurname()
+        {
+            return surname;
+        }
+
+        public int getCourse()
+        {
+            return course;
+        }
     }
 }
