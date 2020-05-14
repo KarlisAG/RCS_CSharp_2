44b8eb76ee8673a9f72dfd5f609b0afab65a2475
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-15 02:14:08 +0300

    Diena 8 MD

---
+++ Day9_MD/Day9_MD/Car.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9_MD
+{
+    class Car
+    {
+        private String color;
+        String brand;
+        int doors;
+
+        public Car(String color, String brand, int doors)
+        {
+            this.color = color;
+            this.brand = brand;
+            this.doors = doors;
+        }
+
+        public void Menu()
+        {
+            List<Car> car = new List<Car>();
+            Console.WriteLine("Sveiki! Ievadiet, ludzu, 5 dazadus auto parametrus");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ievadiet auto krasu");
+                String inColor = Console.ReadLine();
+                Console.WriteLine("Ievadiet auto brandu");
+                String inBrand = Console.ReadLine();
+                Console.WriteLine("Ievadiet auto durvju skaitu");
+                int inDoors = Convert.ToInt32(Console.ReadLine());
+                car.Add(new Car(inColor, inBrand, inDoors));
+
+            }
+
+
+            Console.WriteLine("Vai Jus velaties: ");
+            Console.WriteLine("1 - apskatit auto sarakstu");
+            Console.WriteLine("2 - pievienot auto sarakstam");
+            Console.WriteLine("3 - meklet auto saraksta");
+            Console.WriteLine("0 - iziet no programmas");
+
+            String choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Print();
+                    break;
+                case "2":
+                    AddCar();
+                    break;
+                case "3":
+                    Search();
+                    break;
+                case "0":
+                    break;
+                default:
+                    Console.WriteLine("Nepareiza ievade!");
+                    break;
+            }
+        }
+
+        public void Print()
+        {
+            int i = 0;
+
+        }
+
+        public void AddCar()
+        {
+
+        }
+
+        public void Search()
+        {
+
+        }
+    }
+}
---
+++ Day9_MD/Day9_MD/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day9_MD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            car.Menu();
+            Car.Menu();
+        }
+    }
+}
+/*Izveidot Klasi Car- ar laukiem, kas apraksta krāsu, mašīnas brandu (BMW, AUDI utt) un durvju skaitu.
+Izveidot sarakstu ar objektiem, (5 mašīnas), kuru aizpilda cilvēks
+Uztaisīt metodes, kas- izvada datus par visiem auto, lai var pievienot jaunu auto un, kas meklē pēc filtriem (durvju skaits, mašīnas krāsas utt). Lai lietotājs izvēlas pēc kā meklēt.
+*/
