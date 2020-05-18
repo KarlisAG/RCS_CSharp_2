99b2e9c6d4c0eb4025e8f5bb73d066318961c1a7
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-18 14:44:35 +0300

    Diena 10 nodarbība

---
+++ Day10_MD/Day10_MD/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Day10_MD
+{
+    class Program
+    {
+        static int[] arr = new int[10000];
+        static Stopwatch stopWatch = new Stopwatch();
+        static int[] arr2;
+
+        static void Main(string[] args)
+        {
+            Random();
+            Console.WriteLine("Sakas bubble sort");
+            //BubbleSort();
+            Console.WriteLine("Sakas gnome sort");
+            GnomeSort();
+        }
+        public static void setArr2(int[] a)
+        {
+            arr2 = a;
+        }
+
+        public static int[] getArr2(int[] a)
+        {
+            return a;
+        }
+        public static void Random()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(100000);
+            }
+            setArr2(arr);
+
+            //PrintArray(arr);
+
+        }
+
+        public static void GnomeSort()
+        {
+            Stopwatch watch = stopWatch;
+
+            //PrintArray(arr2);
+            //Console.WriteLine("-----Pirms-----");
+            //PrintArray(arr);
+            watch.Start();
+
+            int g = 0;
+            //Console.WriteLine("Garums: " + arr2.Length);
+            while (g < arr2.Length)
+            {
+                if (g == 0)
+                {
+                    g++;
+                }
+                if (arr2[g] >= arr2[g - 1])
+                {
+                    g++;
+                }
+                else
+                {
+                    int temp = arr2[g];
+                    arr2[g] = arr2[g - 1];
+                    arr2[g - 1] = temp;
+                    g--;
+                }
+            }
+
+            //Console.WriteLine();
+            //Console.WriteLine("-----Pēc-----");
+            //PrintArray(arr2);
+
+            watch.Stop();
+            Console.WriteLine("Aizņemtais laiks ciklam: " + watch.ElapsedMilliseconds + " milisekundes");
+        }
+
+
+        public static void BubbleSort()
+        {
+            //Console.WriteLine("-----Pirms------");
+            //PrintArray(arr);
+
+            stopWatch.Start();
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            //Console.WriteLine();
+            //Console.WriteLine("-----Pēc------");
+            //PrintArray(arr);
+
+            stopWatch.Stop();
+
+            Console.WriteLine("Aizņemtais laiks ciklam: " + stopWatch.ElapsedMilliseconds + " milisekundes");
+            stopWatch.Reset();
+
+        }
+
+
+        public static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
+
--- Diena10_[Algorithms]Algoritmi/Diena10_[Algorithms]Algoritmi/Program.cs
+++ Diena10_[Algorithms]Algoritmi/Diena10_[Algorithms]Algoritmi/Program.cs
@@ -1,52 +1,102 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Diena10__Algorithms_Algoritmi
 {
     class Program
     {
+        static int[] arr = new int[10];
+        static int[] arr2 = new int[10];
+
         static void Main(string[] args)
         {
+            Random();
+            Console.WriteLine("Bubble sort");
+            Console.WriteLine();
             BubbleSort();
+            Random2();
+            Console.WriteLine();
+            Console.WriteLine("Gnome sort");
+            Console.WriteLine();
+            GnomeSort();
+        }
+        public static void Random()
+        {
+            Random random = new Random();
 
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(41);
+            }
+        }
+        public static void Random2()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr2[i] = random.Next(41);
+            }
         }
 
         public static void GnomeSort() 
-        { 
+        {
+            Console.WriteLine("-----Pirms-----");
+            PrintArray(arr2);
+            int g = 0;
+            while (g < arr2.Length) 
+            {
+                if (g == 0)
+                {
+                    g++;
+                }
+                if (arr2[g] >= arr2[g - 1])
+                {
+                    g++;
+                }
+                else
+                {
+                    int temp = arr2[g];
+                    arr2[g] = arr2[g - 1];
+                    arr2[g - 1] = temp;
+                    g--;
+                }
+            }
 
+            Console.WriteLine();
+            Console.WriteLine("-----Pēc-----");
+            PrintArray(arr2);
         }
 
 
         public static void BubbleSort()
         {
-            Random rnd = new Random();
-            int[] rndArray = new int[10];
-
             Console.WriteLine("-----Pirms------");
+            PrintArray(arr);
 
-            for (int i = 0; i < rndArray.Length; i++)
+            for (int i = 0; i < arr.Length - 1; i++)
             {
-                rndArray[i] = rnd.Next(41);//janem 1 vairak, jo ir neieskaitot to skaitli
-                Console.Write(rndArray[i] + " ");
-            }
-
-            for (int i = 0; i < rndArray.Length - 1; i++)
-            {
-                for (int j = 0; j < rndArray.Length - i - 1; j++)
+                for (int j = 0; j < arr.Length - i - 1; j++)
                 {
-                    if (rndArray[j] > rndArray[j + 1])
+                    if (arr[j] > arr[j + 1])
                     {
-                        int temp = rndArray[j];
-                        rndArray[j] = rndArray[j + 1];
-                        rndArray[j + 1] = temp;
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
                     }
                 }
             }
             Console.WriteLine();
             Console.WriteLine("-----Pēc------");
+            PrintArray(arr);
+        }
 
-            for (int i = 0; i < rndArray.Length; i++)
+
+        public static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
             {
-                Console.Write(rndArray[i] + " ");
+                Console.Write(arr[i] + " ");
             }
         }
     }
