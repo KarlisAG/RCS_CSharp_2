7214da1b3bf158b3ad9eb3b443a962b30b97d279
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-25 18:21:29 +0300

    Diena 13 nodarbība

---
+++ Diena13_GUI/Diena13_GUI/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Diena13_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fieldName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.fieldSurname = new System.Windows.Forms.TextBox();
+            this.fieldCourse = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ievadiet studenta vārdu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ievadiet studenta uzvārdu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ievadiet studenta kursu";
+            // 
+            // fieldName
+            // 
+            this.fieldName.Location = new System.Drawing.Point(157, 46);
+            this.fieldName.Name = "fieldName";
+            this.fieldName.Size = new System.Drawing.Size(130, 20);
+            this.fieldName.TabIndex = 4;
+            this.fieldName.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(514, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 5;
+            // 
+            // fieldSurname
+            // 
+            this.fieldSurname.Location = new System.Drawing.Point(157, 82);
+            this.fieldSurname.Name = "fieldSurname";
+            this.fieldSurname.Size = new System.Drawing.Size(130, 20);
+            this.fieldSurname.TabIndex = 6;
+            // 
+            // fieldCourse
+            // 
+            this.fieldCourse.Location = new System.Drawing.Point(157, 117);
+            this.fieldCourse.Name = "fieldCourse";
+            this.fieldCourse.Size = new System.Drawing.Size(130, 20);
+            this.fieldCourse.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(327, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(327, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(327, 120);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 10;
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(56, 320);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(84, 13);
+            this.labelMessage.TabIndex = 11;
+            this.labelMessage.Text = "Lietotne palaista";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(70, 201);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(172, 53);
+            this.btnTest.TabIndex = 12;
+            this.btnTest.Text = "Validet studentu";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 573);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.fieldCourse);
+            this.Controls.Add(this.fieldSurname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.fieldName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox fieldName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox fieldSurname;
+        private System.Windows.Forms.TextBox fieldCourse;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.Button btnTest;
+    }
+}
+
---
+++ Diena13_GUI/Diena13_GUI/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diena13_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Print();
+        }
+        //events -> textchanged - izdzes tur, lai metodi izdēstu
+        public void CheckTextBoxes()
+        {
+            if(fieldName.Text == "")
+            {
+                label4.Text = "laukums nav aizpildīts!";
+                InitializeComponent();
+            }
+            else
+            {
+                label4.Text = "  ";
+            }
+
+            if (fieldSurname.Text == "")
+            {
+                label6.Text = "laukums nav aizpildīts!";
+                InitializeComponent();
+            }
+
+            if (fieldCourse.Text == "")
+            {
+                
+                label7.Text = "laukums nav aizpildīts!";
+                InitializeComponent();
+            }
+        }
+        public void CheckCourse()
+        {
+
+        }
+        public void Print()
+        {
+            labelMessage.Text = String.Format(fieldName + " " + fieldSurname + " "+ fieldCourse);
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            CheckTextBoxes();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            bool valid = true;//valid = ziliga krasa -gaisi
+
+            if (fieldName.Text.Length < 1 && fieldSurname.Text.Length < 1)
+            {
+                valid = false;
+            }
+
+            int course = 0;
+
+            try//rozigi krasu
+            {
+                course = Convert.ToInt32(fieldCourse.Text);
+            }
+            catch
+            {
+                valid = false;
+            }
+
+            if (valid)
+            {
+                labelMessage.Text = "Veiksmigi!";
+            }
+            else
+            {
+                labelMessage.Text = "Nav pareiza ievade";
+            }
+        }
+    }
+}
