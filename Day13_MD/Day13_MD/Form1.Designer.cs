f80a202643efe840ed18e5b4dd226e7fdaaac03b
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-27 13:36:16 +0300

    Diena 13 MD

---
+++ Day13_MD/Day13_MD/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Day13_MD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.txtBoxSurname = new System.Windows.Forms.TextBox();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.txtBoxCourse = new System.Windows.Forms.TextBox();
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(62, 56);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(119, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Ievadiet studenta vārdu";
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Location = new System.Drawing.Point(221, 49);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxName.TabIndex = 1;
+            // 
+            // txtBoxSurname
+            // 
+            this.txtBoxSurname.Location = new System.Drawing.Point(221, 93);
+            this.txtBoxSurname.Name = "txtBoxSurname";
+            this.txtBoxSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxSurname.TabIndex = 3;
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblSurname.Location = new System.Drawing.Point(62, 100);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(130, 13);
+            this.lblSurname.TabIndex = 2;
+            this.lblSurname.Text = "Ievadiet studenta uzvārdu";
+            // 
+            // txtBoxCourse
+            // 
+            this.txtBoxCourse.Location = new System.Drawing.Point(221, 137);
+            this.txtBoxCourse.Name = "txtBoxCourse";
+            this.txtBoxCourse.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxCourse.TabIndex = 5;
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.Location = new System.Drawing.Point(62, 144);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(118, 13);
+            this.lblCourse.TabIndex = 4;
+            this.lblCourse.Text = "Ievadiet studenta kursu";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(112, 209);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(159, 45);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "Pārbaudīt un verificēt";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblOut
+            // 
+            this.lblOut.AutoSize = true;
+            this.lblOut.Location = new System.Drawing.Point(109, 322);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(0, 13);
+            this.lblOut.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 524);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtBoxCourse);
+            this.Controls.Add(this.lblCourse);
+            this.Controls.Add(this.txtBoxSurname);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.txtBoxName);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.TextBox txtBoxSurname;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.TextBox txtBoxCourse;
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblOut;
+    }
+}
+
---
+++ Day13_MD/Day13_MD/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day13_MD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            bool valid = true;
+
+            if(txtBoxName.Text.Length == 0 || txtBoxSurname.Text.Length == 0)
+            {
+                valid = false;
+            }
+
+            try
+            {
+                int number = Convert.ToInt32(txtBoxCourse.Text);
+                if(1 <= number && number <= 3)
+                {
+                    lblOut.Text = "Students validēts! " + "Students, " + txtBoxName.Text  + " " + txtBoxSurname.Text + 
+                        ", studēs " + txtBoxCourse.Text + ". kursā!";
+                }
+                else
+                {
+                    lblOut.Text = "Kursam ir jābūt no 1 līdz 3!";
+                }
+            }
+            catch
+            {
+                lblOut.Text = "Pie kursa ir jāievada cipars!";
+            }
+
+            if (valid == false)
+            {
+                lblOut.Text = "Nepareiza ievade!";
+            }
+        }
+    }
+}
