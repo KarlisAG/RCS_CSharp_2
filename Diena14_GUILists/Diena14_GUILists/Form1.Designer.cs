74b9c49a0675561ffd88d90497a0dfdb2e1bc1c1
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-27 17:47:43 +0300

    Diena 14 nodarbība

--- Diena14_GUILists/Diena14_GUILists/Form1.Designer.cs
+++ Diena14_GUILists/Diena14_GUILists/Form1.Designer.cs
@@ -28,47 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lstTest = new System.Windows.Forms.ListView();
-            this.btnTest = new System.Windows.Forms.Button();
+            this.lst = new System.Windows.Forms.ListView();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtBoxNumber = new System.Windows.Forms.TextBox();
+            this.lblEdit = new System.Windows.Forms.Label();
+            this.txtBoxEdit = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // lstTest
+            // lst
             // 
-            this.lstTest.HideSelection = false;
-            this.lstTest.Location = new System.Drawing.Point(12, 12);
-            this.lstTest.Name = "lstTest";
-            this.lstTest.Size = new System.Drawing.Size(180, 277);
-            this.lstTest.TabIndex = 0;
-            this.lstTest.UseCompatibleStateImageBehavior = false;
-            this.lstTest.View = System.Windows.Forms.View.List;
+            this.lst.HideSelection = false;
+            this.lst.Location = new System.Drawing.Point(12, 12);
+            this.lst.Name = "lst";
+            this.lst.Size = new System.Drawing.Size(174, 246);
+            this.lst.TabIndex = 0;
+            this.lst.UseCompatibleStateImageBehavior = false;
+            this.lst.View = System.Windows.Forms.View.List;
             // 
-            // btnTest
+            // txtBox
             // 
-            this.btnTest.Location = new System.Drawing.Point(222, 12);
-            this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(75, 23);
-            this.btnTest.TabIndex = 1;
-            this.btnTest.Text = "Push me";
-            this.btnTest.UseVisualStyleBackColor = true;
-            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            this.txtBox.Location = new System.Drawing.Point(224, 12);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(100, 20);
+            this.txtBox.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(224, 51);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 25);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(224, 91);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 25);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(224, 133);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(100, 25);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(358, 51);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 5;
+            // 
+            // txtBoxNumber
+            // 
+            this.txtBoxNumber.Location = new System.Drawing.Point(697, 44);
+            this.txtBoxNumber.Name = "txtBoxNumber";
+            this.txtBoxNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxNumber.TabIndex = 7;
+            this.txtBoxNumber.Visible = false;
+            // 
+            // lblEdit
+            // 
+            this.lblEdit.AutoSize = true;
+            this.lblEdit.Location = new System.Drawing.Point(431, 139);
+            this.lblEdit.Name = "lblEdit";
+            this.lblEdit.Size = new System.Drawing.Size(0, 13);
+            this.lblEdit.TabIndex = 8;
+            // 
+            // txtBoxEdit
+            // 
+            this.txtBoxEdit.Location = new System.Drawing.Point(361, 75);
+            this.txtBoxEdit.Name = "txtBoxEdit";
+            this.txtBoxEdit.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxEdit.TabIndex = 9;
+            this.txtBoxEdit.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnTest);
-            this.Controls.Add(this.lstTest);
+            this.Controls.Add(this.txtBoxEdit);
+            this.Controls.Add(this.lblEdit);
+            this.Controls.Add(this.txtBoxNumber);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.lst);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListView lstTest;
-        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.ListView lst;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtBoxNumber;
+        private System.Windows.Forms.Label lblEdit;
+        private System.Windows.Forms.TextBox txtBoxEdit;
     }
 }
 
--- Diena14_GUILists/Diena14_GUILists/Form1.cs
+++ Diena14_GUILists/Diena14_GUILists/Form1.cs
@@ -16,17 +16,102 @@ namespace Diena14_GUILists
         {
             InitializeComponent();
         }
-        //.ToCharArray - pārveido stringu uz char masīvu!!
-        private void btnTest_Click(object sender, EventArgs e)
-        {
-            lstTest.View = View.Details;
-            lstTest.Items.Add("elements");//strādā kā parasts sarkast
-            //skaitlī ir jāparveido uz stringu, savādāk sarakstā neņems
-            lstTest.Items[0].SubItems.Add("aa");//kas ir subitems???
-            lstTest.Items[0].SubItems.Add("bb");
-            //subitems ir kā kkādas detaļas; vērtības, kuras sadalītas ar komatu; var iedomāties kā saturu ar punktiem un apakšpunktiem
 
-            lstTest.Items.Add(Convert.ToString(2));
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtBox.Text.Length == 0)
+            {
+
+            }
+            else
+            {
+                lst.Items.Add(txtBox.Text);
+            }
         }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (txtBoxNumber.Text.Length == 0)
+            {
+                if (txtBox.Text.Length == 0)
+                {
+
+                }
+                else
+                {
+                    txtBoxNumber.Visible = true;
+                    lblMessage.Text = "Ievadiet elemta indeksu, kuru velaties izdzest spiediet Delete!";
+                    
+                }
+            }
+            else
+            {
+                lblMessage.Text = "Ievadiet elemta indeksu, kuru velaties izdzest un spiediet Delete!";
+                txtBoxNumber.Visible = true;
+                try
+                {
+                    int choice = Convert.ToInt32(txtBoxNumber.Text);
+                    lst.Items[choice - 1].Remove();
+                }
+                catch
+                {
+                    lblMessage.Text = "Jums ir jāievada cipars!";
+                }
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (txtBoxNumber.Text.Length == 0)
+            {
+                if (txtBox.Text.Length == 0)
+                {
+
+                }
+                else
+                {
+                    txtBoxNumber.Visible = true;
+                    lblMessage.Text = "Ievadiet elemta indeksu, kuru velaties rediget un spiediet Edit!";
+                }
+            }
+            else
+            {
+                lblMessage.Text = "Ievadiet elemta indeksu, kuru velaties rediget un spiediet Edit!";
+                txtBoxNumber.Visible = true;
+                try
+                {
+                    int choice = Convert.ToInt32(txtBoxNumber.Text);
+                    lblMessage.Text = "Ievadiet jauna elementa tekstu zemak!";
+                    txtBoxEdit.Visible = true;
+                    lst.Items[choice - 1].Text = txtBoxEdit.Text;
+                }
+                catch
+                {
+
+                }
+
+            }
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        //.ToCharArray - pārveido stringu uz char masīvu!!
+        //private void btnTest_Click(object sender, EventArgs e)
+        //{
+        //    lstTest.View = View.Details;//nosaka kā uz lietām skatās/tiek siplajotas?!
+        //    lstTest.Items.Add("elements");//strādā kā parasts sarkast
+        //    //skaitlī ir jāparveido uz stringu, savādāk sarakstā neņems
+        //    lstTest.Items[0].SubItems.Add("aa");//kas ir subitems???
+        //    lstTest.Items[0].SubItems.Add("bb");
+        //    //subitems ir kā kkādas detaļas; vērtības, kuras sadalītas ar komatu; var iedomāties kā saturu ar punktiem un apakšpunktiem
+
+        //    lstTest.Items.Add(Convert.ToString(2));
+        //}
+
+
+
     }
 }
