0e0fa477015608c991881f856d03b9de4ce68530
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-05-29 01:42:02 +0300

    Diena 14 MD

---
+++ Day14_MD/Day14_MD/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Day14_MD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstMain = new System.Windows.Forms.ListView();
+            this.lstBackup = new System.Windows.Forms.ListView();
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.txtChoice = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstMain
+            // 
+            this.lstMain.HideSelection = false;
+            this.lstMain.Location = new System.Drawing.Point(12, 12);
+            this.lstMain.Name = "lstMain";
+            this.lstMain.Size = new System.Drawing.Size(169, 260);
+            this.lstMain.TabIndex = 0;
+            this.lstMain.UseCompatibleStateImageBehavior = false;
+            this.lstMain.View = System.Windows.Forms.View.List;
+            // 
+            // lstBackup
+            // 
+            this.lstBackup.HideSelection = false;
+            this.lstBackup.Location = new System.Drawing.Point(667, 341);
+            this.lstBackup.Name = "lstBackup";
+            this.lstBackup.Size = new System.Drawing.Size(121, 97);
+            this.lstBackup.TabIndex = 1;
+            this.lstBackup.UseCompatibleStateImageBehavior = false;
+            // 
+            // txtIn
+            // 
+            this.txtIn.Location = new System.Drawing.Point(187, 12);
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(100, 20);
+            this.txtIn.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(187, 38);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 26);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(187, 70);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 26);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(187, 102);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(100, 26);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(187, 134);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(100, 26);
+            this.btnUndo.TabIndex = 6;
+            this.btnUndo.Text = "Undo";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(290, 15);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(296, 13);
+            this.lblInfo.TabIndex = 7;
+            this.lblInfo.Text = "Izvelaties kadu darbibu velaties veikt, tad sekojiet instrukcijai!";
+            this.lblInfo.Click += new System.EventHandler(this.lblInfo_Click);
+            // 
+            // txtChoice
+            // 
+            this.txtChoice.Location = new System.Drawing.Point(293, 106);
+            this.txtChoice.Name = "txtChoice";
+            this.txtChoice.Size = new System.Drawing.Size(100, 20);
+            this.txtChoice.TabIndex = 8;
+            this.txtChoice.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtChoice);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtIn);
+            this.Controls.Add(this.lstBackup);
+            this.Controls.Add(this.lstMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstMain;
+        private System.Windows.Forms.ListView lstBackup;
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TextBox txtChoice;
+    }
+}
+
---
+++ Day14_MD/Day14_MD/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day14_MD
+{
+    public partial class Form1 : Form
+    {
+        int AddTimes = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (AddTimes == 0)
+            {
+                lblInfo.Text = "Ierakstiet kreisaja loga elementu, kuru velaties pievienot sarakstam un tad atkal spiediet Add!";
+            }
+            else
+            {
+
+                if (txtIn.Text == "")
+                {
+
+                }
+                else
+                {
+                    bool contains = false;
+                    if(lstMain.Items.Count == 0)
+                    {
+                        lstMain.Items.Add(txtIn.Text);
+                        lblInfo.Text = "Elements pievienots!";
+                    }
+                    else
+                    {
+                        for (int i = 0; i < lstMain.Items.Count; i++)
+                        {
+                        //taisit citu ciklu/ klat likt masivu?
+                            if (lstMain.Items[i].Text == txtIn.Text)
+                            {
+                                lblInfo.Text = "Tads elements jau atrodas saraksta!";
+                                contains = true;
+                            }
+                        }
+                        if (!contains)
+                        {
+                            lstMain.Items.Add(txtIn.Text);
+                            lblInfo.Text = "Elements pievienots!";
+                        }
+                        
+                    }
+                    
+                    
+                }
+            }
+            AddTimes++;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int times = 0;
+            if (times == 0)
+            {
+                lblInfo.Text = "Ar kursoru iezimejiet elementu saraksta, kuru velaties izdzest un tad spiediet Delete";
+            }
+            else
+            {
+                foreach (ListViewItem item in lstMain.SelectedItems)
+                {
+                    item.Remove();
+                }
+            }
+            times++;
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListCopy()
+        {
+            for(int i = 0; i < lstMain.Items.Count; i++)
+            {
+                lstBackup = lstMain;
+            }
+        }
+
+        private void lblInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+/* pasniedzēja variants
+        pie add
+
+       if (inputBox.Text != ""){
+        lstElements.Items.Add(inputBox.Text)}
+
+        pie delete
+        foreach(ListViewItem item in lstElements.SelectedItems)
+        {
+        //piekļūst pie aktra itema kas ir selektots - kas ir pie lista, pats cilvēks var iekrāsot un tad uzspiest uz pogu
+        item.Remove();
+        }
+
+        pie edit
+
+        lstElements.SelectedItems[0].Text = inputBox.Text;
+        */
