1e72cdf5fb51a5f9d39f447478c09c3b8500c43d
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-06-10 02:33:40 +0300

    Diena 18 MD

---
+++ Day18_MD/Day18_MD/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Day18_MD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String url = "https://my-json-server.typicode.com/KarlisAG/FakeRest/data";
+            
+            WebClient client = new WebClient();
+
+            String response = client.DownloadString(url);
+
+            Console.WriteLine(response);
+
+            //TempData rec = JsonConvert.DeserializeObject<TempData>(response);
+
+            //if (rec.status == "success")
+            //{
+            //    foreach (Student s in rec.st)
+            //    {
+            //        Console.WriteLine(s.name + " " + s.getSurname() + " " + s.getCourse());
+            //    }
+            //}
+        }
+    }
+}
---
+++ Day18_MD/Day18_MD/TempData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day18_MD
+{
+    class TempData
+    {
+        public String status;
+        public List<Student> st;
+
+        public TempData(String status)
+        {
+            this.status = status;
+        }
+    }
+}
