971923fa6c8bf93081426043d3f0979eaae7cb07
KarlisAG <64302464+KarlisAG@users.noreply.github.com>
2020-06-15 17:33:19 +0300

    Diena 21 nodarbība

---
+++ Day21_SQL/Day21_SQL/DBConnection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day21_SQL
+{
+    class DBConnection
+    {
+    }
+}
---
+++ Diena21_SQL/SQLDB.sql
@@ -0,0 +1,29 @@
+USE ofiss;
+
+CREATE TABLE employees
+( 
+employeeId INT,
+employeeName varchar(20),
+employeeLastName varchar(20) 
+);
+
+DROP TABLE employees;
+
+
+ALTER TABLE employees
+MODIFY COLUMN employeeId INT PRIMARY KEY AUTO_INCREMENT; 
+
+
+INSERT INTO employees ( employeeName, employeeLastName)
+VALUES ("Juris", "Berzins"),
+("Anete", "Cauna");
+
+INSERT INTO employees(employeeName, employeeLastName)
+VALUES("Karls", "Markss"),
+("Maris", "Pagals");
+
+SELECT employeeName, employeeLastName 
+FROM employees
+WHERE employeeId = 1;
+
+SELECT * FROM employees;
